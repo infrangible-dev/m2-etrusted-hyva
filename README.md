2180cb81c7ef2a74144c3b01e607cca3828d150a
Andreas Knollmann <a.knollmann@seak.dev>
2024-08-13 01:02:51 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# Infrangible eTrusted Hyvä Theme
+
+## Introduction
+
+This module adds compatibility between Infrangible eTrusted and Hyvä theme.
+
+## License
+
+Infrangible eTrusted Hyvä is licensed under the MIT License - see the LICENSE file for details.
---
+++ src/view/frontend/tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * @author      Andreas Knollmann
+ * @copyright   2014-2024 Softwareentwicklung Andreas Knollmann
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT
+ */
+
+module.exports = {
+    purge: {
+        content: [
+            '../**/*.phtml',
+        ]
+    }
+}
